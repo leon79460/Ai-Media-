113aabdaa891db72c02771b870ebb4bdb6a205e1
Ash Sams Md Tanvir Jahan <tanveerja98@gmail.com>
2026-06-01 19:32:39 +0600

    feat: Update reveal-motion styles for improved visibility; add .htaccess for cache control settings

--- next.config.mjs
+++ next.config.mjs
@@ -2,28 +2,10 @@
 const nextConfig = {
   /* config options here */
   devIndicators: false,
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'no-store, no-cache, max-age=0, must-revalidate',
-          },
-          {
-            key: 'Pragma',
-            value: 'no-cache',
-          },
-          {
-            key: 'Expires',
-            value: '0',
-          },
-        ],
-      },
-    ];
-  },
+  output: 'export',
+  trailingSlash: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
